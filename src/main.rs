6e65c8e09e545093e59674342b53dbc3ec4764b3
Victor <zedfromionia@gmail.com>
2020-05-19 19:18:58 -0300

    change from for to iterators

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
+
         process::exit(1);
     });
 
@@ -15,4 +14,3 @@ fn main() {
         process::exit(1);
     }
 }
-
